1609d2f9d7b4d0afa66adf72fe3f64e8b3a0befa
SantiagoTZ8899 <santiago.tz89@gmail.com>
2020-01-04 13:57:51 -0600

    Updates

---
+++ precache-manifest.d49a0b9f591a910e6917fe07c1457ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33874ea0fe9f5f7e33c708a61d203086",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "efa29a7217bf77c02546",
+    "url": "/Clicky-Game/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a5edf9e02357924fb145",
+    "url": "/Clicky-Game/static/css/main.7b98315e.chunk.css"
+  },
+  {
+    "revision": "efa29a7217bf77c02546",
+    "url": "/Clicky-Game/static/js/2.12a31b0d.chunk.js"
+  },
+  {
+    "revision": "a5edf9e02357924fb145",
+    "url": "/Clicky-Game/static/js/main.0784cd7f.chunk.js"
+  },
+  {
+    "revision": "bc619729ebbeac64ee16",
+    "url": "/Clicky-Game/static/js/runtime-main.dfa503e4.js"
+  }
+]);
